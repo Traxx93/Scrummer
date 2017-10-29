7cebabb2ff4a417042a3b133a133117a55b5fc7a
Traxx93 <danilogrujicicfax@gmail.com>
2017-10-29 20:07:19 +0100

    Added WorkItem to Entities

---
+++ Scrumer/Infrastrucutre/Entities/WorkItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Scrumer.Infrastrucutre.Entities
+{
+    public class WorkItem
+    {
+        [Key]
+        public int WorkItemID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+    }
+}
